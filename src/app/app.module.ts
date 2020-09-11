1d26fffc014506f3d0e4573649db2fa27f43a172
shayandaneshvar <daneshvarshayan@gmail.com>
2020-09-11 23:16:13 +0430

    Add Cors Support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RegisterComponent } from './auth/register/register.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterSuccessComponent} from './auth/register-success/register-success.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    RegisterSuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: 'register', component: RegisterComponent}
+    ]),
+    HttpClientModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {RegisterPayload} from './register-payload';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  registerPayload: RegisterPayload;
 
-  constructor() { }
+  constructor(private authService: AuthService, private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    });
+    this.registerPayload = {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.registerPayload.username = this.registerForm.get('username').value;
+    this.registerPayload.email = this.registerForm.get('email').value;
+    this.registerPayload.password = this.registerForm.get('password').value;
+    this.registerPayload.confirmPassword = this.registerForm.get('confirmPassword').value;
+    this.authService.register(this.registerPayload)
+      .subscribe(data => console.log('register success'), error => console.log('register failed'));
+  }
 }
